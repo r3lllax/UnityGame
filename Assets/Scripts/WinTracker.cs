7d0b17e0903daba9120dfda4080f57299d88f20f
r3lllax <turkevicaleksandr@gmail.com>
2025-02-13 22:40:32 +0300

    mini commit

--- Assets/Scripts/WinTracker.cs
+++ Assets/Scripts/WinTracker.cs
@@ -3,13 +3,10 @@ using UnityEngine;
 public class WinTracker : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
         if(PlayerDataManager.Instance.playerData.AvalibleDoors.Count==0){
             //Загружем сцену финала игры
